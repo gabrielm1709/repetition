97dc4f19e924543095cd44405e33b4e18acff13f
gabrielm1709 <gabriel.morck@elev.ga.ntig.se>
2022-08-31 09:43:11 +0200

    Update changes

--- Program.cs
+++ Program.cs
@@ -50,12 +50,65 @@ namespace repetition
             int seconds = days * 24 * 3600;
             Console.WriteLine(seconds);
             
-          
+
+        
+            // Uppgift 30
+            Console.Write("Give me your percent: ");
+            int percent = int.Parse(Console.ReadLine());
+            int score = 0;
+
+            if(percent <= 49) 
+            {
+                score = 0;
+            }
+
+            if(percent >= 50 && percent <= 59) 
+            {
+                score = 1;
+            }
+
+            if (percent >= 60 && percent <= 69)
+            {
+                score = 2;
+            }
+
+            if (percent >= 70 && percent <= 79)
+            {
+                score = 3;
+            }
+
+            if (percent >= 80 && percent <= 89)
+            {
+                score = 4;
+            }
+
+            if (percent >= 90 && percent <= 100)
+            {
+                score = 5;
+            }
+
+            else 
+            {
+                Console.WriteLine("Impossible");
+            }
+
+            Console.WriteLine($"Your score: {score}");
+
+            // Uppgift 31
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("oDD");
+            }
+
             // Uppgift 36
             int num = int.MinValue;
-            int sum = 0;
-            int cnt = -1;
-
+      
             while(num != 0) 
             {
                 num = int.Parse(Console.ReadLine());
@@ -67,8 +120,8 @@ namespace repetition
 
             // Uppgift 40
             num = int.MinValue;
-            sum = 0;
-            cnt = -1;
+            int sum = 0;
+            int cnt = -1;
             while(num != 0) 
             {
                 num = int.Parse(Console.ReadLine());
@@ -76,8 +129,6 @@ namespace repetition
                 cnt += 1;
             }
             Console.WriteLine($"Summa: {sum}, antal: {cnt}");
-
-
         }
     }
 }
